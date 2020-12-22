6b1976147d1fe106822af3775d1cdd0d1de01450
Aluwaine <tanakashark@gmail.com>
2020-12-22 10:05:20 +0200

    conditions

--- index.js
+++ index.js
@@ -2,3 +2,15 @@ var msg = "Hello, world!";
 alert(msg);
 console.log("Hello, console!");
 document.write("Hello, document!");
+
+var num1 = 804;
+
+if(num1 % 2 == 0)
+{
+  document.write("num1 is even");
+}
+else{
+  document.write("num1 is odd");
+}
+
+var num2 = 99;
