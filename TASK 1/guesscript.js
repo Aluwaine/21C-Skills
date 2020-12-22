045f00c836aeabe3ac91e72cf2b4f4ac9702cff9
Aluwaine <tanakashark@gmail.com>
2020-12-22 11:31:33 +0200

    guess

---
+++ TASK 1/guesscript.js
@@ -0,0 +1,27 @@
+// program data:
+var number = 4;
+var guess;
+var limit = 5;
+var won =  false;
+
+for (i = 1; i <= limit; i++){
+    // prompt user for their guess 
+    guess = prompt("Guess a number");
+
+    // if correct: let the user know they won
+    if(guess == number){
+        document.write("Correct! You won.");
+        won = true;
+        break;
+    }
+    // if incorrect: let the user know how many tries they have remaining
+    else{
+        alert("incorrect. \nTries remaining: " + (limit - i)");
+    }
+}
+
+// if the user ran out of tries, let them know the game is over
+if(!won){
+    document.write("Sorry, you ran out of tries. Game over.");
+}
+
